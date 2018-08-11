147d60c6a008a42eb1aceb3553b939f50d3c8124
rspears417 <racheal.spears417@gmail.com>
2018-08-11 17:33:50 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+let friends =
+               [ {
+                "name":"Ahmed",
+                "photo":"./images/Ahmed.jpg",
+                "scores":[
+                    5,
+                    1,
+                    4,
+                    4,
+                    5,
+                    1,
+                    2,
+                    5,
+                    4,
+                    1
+                ]
+            }, {
+                "name": "Tanner",
+                "photo": "./images/tanner.png",
+                "scores":[
+                    5,
+                    2,
+                    2,
+                    3,
+                    5,
+                    1,
+                    4,
+                    5,
+                    4,
+                    4
+                ]
+            },
+            {
+                "name": "LeBron",
+                "photo": "./images/lebron.png",
+                "scores":[
+                    1,
+                    3,
+                    2,
+                    1,
+                    5,
+                    5,
+                    2,
+                    5,
+                    2,
+                    1
+                ]
+            },
+            {
+                "name": "Patrick Mahomes",
+                "photo": "./images/patty.jpg",
+                "scores":[
+                    1,
+                    5,
+                    2,
+                    2,
+                    5,
+                    1,
+                    2,
+                    5,
+                    4,
+                    5
+                ]
+            },
+            {
+                "name": "Tyreek Hill",
+                "photo": "./images/cheetah.jpeg",
+                "scores":[
+                    1,
+                    1,
+                    1,
+                    1,
+                    5,
+                    5,
+                    5,
+                    5,
+                    4,
+                    5
+                ]
+            }
+        ]
+
+
+module.exports = friends;
